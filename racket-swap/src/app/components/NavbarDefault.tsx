975646d9786df34888214301d3889c8d033f7152
Brian Yang <brianyang120488@gmail.com>
2025-05-23 16:45:53 -0400

    changed positions and sizing of navbar items

--- racket-swap/src/app/components/NavbarDefault.tsx
+++ racket-swap/src/app/components/NavbarDefault.tsx
@@ -3,22 +3,11 @@
 import React from "react";
 import {
   Navbar,
-  Collapse,
   Typography,
   Button,
-  IconButton,
 } from "@material-tailwind/react";
  
 export function NavbarDefault() {
-  const [openNav, setOpenNav] = React.useState(false);
- 
-  React.useEffect(() => {
-    window.addEventListener(
-      "resize",
-      () => window.innerWidth >= 960 && setOpenNav(false),
-    );
-  }, []);
- 
   const navList = (
     <ul className="mt-2 mb-4 flex flex-col gap-2 lg:mb-0 lg:mt-0 lg:flex-row lg:items-center lg:gap-6">
       <Typography
@@ -99,69 +88,37 @@ export function NavbarDefault() {
           />
         </svg>
         <a href="#" className="flex items-center">
-          Docs
+          About Us
         </a>
       </Typography>
     </ul>
   );
  
   return (
-    <Navbar className="mx-auto px-2 py-2 lg:px-4 lg:py-4">
+    <Navbar className="mx-auto px-2 py-2 lg:px-16 lg:py-4">
       <div className="flex items-center justify-between text-blue-gray-900 border">
         <img
-          className="h-8 object-cover object-center"
+          className="h-10 object-cover object-center"
           src="corner-white.png"
         ></img>
         <div className="hidden lg:block">{navList}</div>
-        <div className="flex items-center gap-x-1">
-          <Button variant="text" size="sm" className="hidden lg:inline-block">
-            <span>Log In</span>
-          </Button>
-          <Button
-            variant="gradient"
-            size="sm"
-            className="hidden lg:inline-block"
+        <Button variant="outlined" className="rounded-full flex items-center gap-2 lg:px-8 lg:py-3">
+          Sign in{" "}
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={2}
+            stroke="currentColor"
+            className="h-5 w-5"
           >
-            <span>Sign in</span>
-          </Button>
-        </div>
-        <IconButton
-          variant="text"
-          className="ml-auto h-6 w-6 text-inherit hover:bg-transparent focus:bg-transparent active:bg-transparent lg:hidden"
-          ripple={false}
-          onClick={() => setOpenNav(!openNav)}
-        >
-          {openNav ? (
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              className="h-6 w-6"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-              strokeWidth={2}
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M6 18L18 6M6 6l12 12"
-              />
-            </svg>
-          ) : (
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="h-6 w-6"
-              fill="none"
-              stroke="currentColor"
-              strokeWidth={2}
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M4 6h16M4 12h16M4 18h16"
-              />
-            </svg>
-          )}
-        </IconButton>
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M17.25 8.25L21 12m0 0l-3.75 3.75M21 12H3"
+            />
+          </svg>
+        </Button>
       </div>
     </Navbar>
   );
