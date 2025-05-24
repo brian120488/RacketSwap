8b9e1ab5a2e6f17b6a8ab040d627280f4daeca2b
Brian Yang <brianyang120488@gmail.com>
2025-05-23 23:49:55 -0400

    Create SignInButton.tsx

---
+++ racket-swap/src/app/components/SignInButton.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+
+// interface SignInButtonProps {
+//   onClick: () => void;
+//   label: string;
+// }
+
+import { Button } from "@material-tailwind/react";
+
+export function SignInButton() {
+  return (
+    <Button variant="outlined" className="rounded-full flex items-center gap-2 lg:px-8 lg:py-3">
+      Sign in
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+        strokeWidth={2}
+        stroke="currentColor"
+        className="h-5 w-5"
+      >
+        <path
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          d="M17.25 8.25L21 12m0 0l-3.75 3.75M21 12H3"
+        />
+      </svg>
+    </Button>
+  );
+};
