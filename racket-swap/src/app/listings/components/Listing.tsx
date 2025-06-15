edc13d254f0021b757cb042a721395e926d26938
Brian Yang <brianyang120488@gmail.com>
2025-06-15 17:45:55 -0400

    Added dollar sign to listing

--- racket-swap/src/app/listings/components/Listing.tsx
+++ racket-swap/src/app/listings/components/Listing.tsx
@@ -16,7 +16,7 @@ export const Listing: React.FC<ListingProps> = ({ image, title, cost, lister, de
       />
       <div className="p-4">
         <h2 className="text-lg font-semibold text-gray-800">{title}</h2>
-        <p className="text-gray-600 mt-2">{cost}</p>
+        <p className="text-gray-600 mt-2">${cost}</p>
       </div>
     </div>
   );
