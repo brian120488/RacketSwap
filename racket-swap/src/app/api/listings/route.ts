ff47929b52a82f341ed9ede49fd9652793fee003
Brian Yang <brianyang120488@gmail.com>
2025-06-15 17:04:41 -0400

    Implemented S3 functionanaliy

--- racket-swap/src/app/api/listings/route.ts
+++ racket-swap/src/app/api/listings/route.ts
@@ -4,7 +4,6 @@ import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
 
 const uri = process.env.DB_URI || "";
 const client = new MongoClient(uri);
-const s3Client = new S3Client({ region: process.env.AWS_REGION })
 
 export async function GET() {
   try {
@@ -17,15 +16,18 @@ export async function GET() {
     const listings = await collection.find({}).toArray();
 
     // Retrieve S3 image URLS
+    const s3 = new S3Client({ region: process.env.AWS_REGION })
     const command = new GetObjectCommand({
-      Bucket: process.env.AWS_BUCKET_NAME,
-      Key: "listings/badminton.jpg", // The path to the file in your S3 bucket
+      Bucket: process.env.AWS_BUCKET_NAME!,
+      Key: 'listings/badminton.jpg',
     });
-    const url = await getSignedUrl(s3Client, command, { expiresIn: 3600 });
+    const url = await getSignedUrl(s3, command);
 
+    listings[0].image = url;
+    console.log(url);
     // Return the listings as a JSON response
     return new Response(
-      JSON.stringify({ url, listings }),
+      JSON.stringify({ listings }),
       { status: 200, headers: { "Content-Type": "application/json" } }
     );
   } catch (error) {
--- racket-swap/src/app/listings/page.tsx
+++ racket-swap/src/app/listings/page.tsx
@@ -26,13 +26,22 @@ export default function Listings() {
       <NavbarDefault />
       <div className="flex justify-center mt-6">
         <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-5 gap-8">
-          {listings.map((listing, index) => (
+          {/* {listings.map((listing, index) => (
             <Listing
               key={index}
               image={listing.image}
               title={listing.title}
               cost={listing.cost}
               lister={listing.lister}
+            />
+          ))} */}
+          {Array.from({ length: 10 }).map((_, index) => (
+            <Listing
+              key={index}
+              image={listings[0]?.image || "https://via.placeholder.com/150"}
+              title={listings[0]?.title || "Default Title"}
+              cost={listings[0]?.cost || "Default Cost"}
+              lister={listings[0]?.lister || "Default Lister"}
             />
           ))}
         </div>
