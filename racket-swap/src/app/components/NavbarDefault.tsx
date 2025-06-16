2cc09f9277f6fa5e0c8a21ab3a2429aa9d3b5a83
Brian Yang <brianyang120488@gmail.com>
2025-06-16 00:41:50 -0400

    Fixed bug with background variable of navbar

--- racket-swap/src/app/components/NavbarDefault.tsx
+++ racket-swap/src/app/components/NavbarDefault.tsx
@@ -9,7 +9,11 @@ import { SearchBar } from "./SearchBar";
 import { SignInButton } from "./SignInButton";
 import Link from "next/link";
  
-export const NavbarDefault = () => {
+interface NavbarDefaultProps {
+  className?: string; // Optional className prop
+}
+ 
+export const NavbarDefault: React.FC<NavbarDefaultProps> = ({ className }) => {
   const navList = (
     <ul className="mt-2 mb-4 flex flex-col gap-2 lg:mb-0 lg:mt-0 lg:flex-row lg:items-center lg:gap-6">
       <Typography
@@ -56,7 +60,9 @@ export const NavbarDefault = () => {
   );
  
   return (
-    <Navbar className="mx-auto px-2 py-2 lg:px-16 lg:py-4 bg-[--background] border-none">
+    <Navbar
+      className={`${className || ""} mx-auto px-2 py-2 lg:px-16 lg:py-4 bg-background border-none`}
+    >
       <div className="h-full flex items-center justify-left text-blue-gray-900">
         <Link href="/">
           <img
